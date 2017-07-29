23349347a0d88af6e3a8a0eea0f99d1cc9dabcbe
Chirag Awale <chiragawale777@gmail.com>
2017-07-28 20:17:16 -0500

    Listview replaced with Recycler view for better performance

--- Folinsight/app/src/main/java/com/chiragawale/folinsight/SearchActivity.java
+++ Folinsight/app/src/main/java/com/chiragawale/folinsight/SearchActivity.java
@@ -4,53 +4,64 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.chiragawale.folinsight.adapter.UserAdapter;
+import com.chiragawale.folinsight.adapter.UserAdapterRV;
 import com.chiragawale.folinsight.entity.Users;
 import com.chiragawale.folinsight.keys.GlobalVar;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.user_list);
         //Getting data from the Intent for searching
         Intent intent = getIntent();
         Uri query = intent.getData();
         setTitle("Results: ");
+
         //Getting refrence of the progress bar
         ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar);
         progressBar.setVisibility(View.VISIBLE);
         //Setting up the adapter
-        final UserAdapter mUserAdapter = new UserAdapter(this, GlobalVar.userDao.getSearchResultList(query.toString()));
 
-        //Getting reference for listview and setting it up
-        ListView listView = (ListView) findViewById(R.id.follower_list);
-        listView.setAdapter(mUserAdapter);
-        View emptyView = findViewById(R.id.empty_view);
-        listView.setEmptyView(emptyView);
-        //Changing the text to suit the class
-        TextView subheadingEmptyView = (TextView)  findViewById(R.id.empty_subtitle_text);
-        subheadingEmptyView.setText("No results found");
-        progressBar.setVisibility(View.INVISIBLE);
+        List<Users> usersList = GlobalVar.userDao.getSearchResultList(query.toString());
 
-        //Setting up intent to open profile of selected user from list
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Users selectedUsers = mUserAdapter.getItem(position);
-                //Converting the profile link String to URI and setting the intent up to open the URI accordingly
-                Uri uri = Uri.parse(selectedUsers.getProfileLink());
-                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(intent);
-            }
-        });
+        UserAdapterRV mUserAdapter = new UserAdapterRV(usersList.size(),usersList);
+
+        RelativeLayout emptyView = (RelativeLayout) findViewById(R.id.empty_view);
+        //Progress bar
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        //gets reference for the recycler view
+        RecyclerView mUserList = (RecyclerView) findViewById(R.id.rv_user_list);
+
+        //Defines the layout for the recycler view
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        mUserList.setLayoutManager(layoutManager);
+
+        //Improves performance
+        mUserList.setHasFixedSize(true);
+        //Sets the adapter for the Recycler view
+        mUserList.setAdapter(mUserAdapter);
+        //Kicks off the loader
+        mUserList.setVisibility(View.GONE);
+
+        if(usersList.size()!=0){
+            emptyView.setVisibility(View.GONE);
+            mUserList.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.GONE);
+        }
     }
 
     @Override
---
+++ Folinsight/app/src/main/java/com/chiragawale/folinsight/adapter/UserAdapterRV.java
@@ -0,0 +1,96 @@
+package com.chiragawale.folinsight.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.chiragawale.folinsight.MainActivity;
+import com.chiragawale.folinsight.R;
+import com.chiragawale.folinsight.entity.Users;
+
+import java.util.List;
+
+/**
+ * Created by chira on 7/28/2017.
+ */
+
+public class UserAdapterRV extends RecyclerView.Adapter<UserAdapterRV.UserViewHolder> {
+    private static int viewHolderCount;
+    private int mNumberItems;
+    private List<Users> mUserList;
+    private Context context;
+
+    public UserAdapterRV(int numberOfItems, List<Users> userList) {
+        mNumberItems = numberOfItems;
+        viewHolderCount = 0;
+        mUserList = userList;
+
+    }
+
+    @Override
+    public UserAdapterRV.UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        int layoutIdForListItem = R.layout.list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        //Inflates a view to pass to viewholder,
+        View view = inflater.inflate(layoutIdForListItem, parent, false);
+        UserViewHolder userViewHolder = new UserViewHolder(view);
+        //Keeps count of the viewholders
+        viewHolderCount++;
+        return userViewHolder;
+    }
+    //Binds data
+    @Override
+    public void onBindViewHolder(UserAdapterRV.UserViewHolder holder, int position) {
+        holder.bind(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNumberItems;
+    }
+
+
+    public class UserViewHolder extends RecyclerView.ViewHolder {
+        //Defining TextViews and ImageViews
+        TextView follower_name;
+        TextView follower_userName;
+        TextView follower_track_date;
+        TextView follower_likes;
+        TextView follower_comments;
+        ImageView follower_profile_picture;
+
+
+        public UserViewHolder(View listItemView) {
+            super(listItemView);
+            //Getting the reference of the views to change their values
+            follower_name = (TextView) listItemView.findViewById(R.id.follower_name);
+            follower_userName = (TextView) listItemView.findViewById(R.id.follower_user_name);
+            follower_track_date = (TextView) listItemView.findViewById(R.id.follower_tracked_date);
+            follower_likes = (TextView) listItemView.findViewById(R.id.likes);
+            follower_comments = (TextView) listItemView.findViewById(R.id.comments);
+            follower_profile_picture = (ImageView) listItemView.findViewById(R.id.profile_picture);
+        }
+
+
+        void bind(int position) {
+            //Binds the data with the views referenced in the constructor
+            Users currentUser = mUserList.get(position);
+            //Setting the texts of their respective textViews according to the data received from the List
+            follower_name.setText(currentUser.getFullName());
+            follower_userName.setText(currentUser.getUserName());
+            // follower_track_date.setText(currentUser.getDate_trackedFrom());
+            follower_comments.setText("Comments : " + String.valueOf(currentUser.getCommentsPosted()));
+            follower_likes.setText("Likes : " + String.valueOf(currentUser.getLikesPosted()));
+            //Loads images from Instagram site
+            Glide.with(context).load(currentUser.getProfilePictureLink()).skipMemoryCache(true).override(144, 144).into(follower_profile_picture);
+
+        }
+    }
+}
