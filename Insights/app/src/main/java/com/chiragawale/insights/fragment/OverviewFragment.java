ae4f28c8376cb25e1bc692044f93730c0f9d94d0
Chirag Awale <chiragawale777@gmail.com>
2017-08-10 21:21:45 -0500

    Databinding used for binding

--- Insights/app/src/main/java/com/chiragawale/insights/fragment/OverviewFragment.java
+++ Insights/app/src/main/java/com/chiragawale/insights/fragment/OverviewFragment.java
@@ -1,5 +1,6 @@
 package com.chiragawale.insights.fragment;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,68 +8,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.chiragawale.insights.databinding.FragmentOverviewBinding;
 import com.chiragawale.insights.keys.GlobalVar;
 import com.chiragawale.insights.R;
 
 
 public class OverviewFragment extends Fragment {
-    public static TextView totalPosts,totalLikes,totalComments,
-    fanLikes, fanComments, followsLikes, followsComments,
-    strangerLikes,strangerComments,fansCount,followsCount,mutualCount,mutualLikes,mutualComments;
-
+   //For binding values to the text views
+    static FragmentOverviewBinding mBinding;
 
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_overview,container,false);
-        //For values for total:
-        totalPosts = (TextView) rootView.findViewById(R.id.total_posts);
-        totalLikes = (TextView) rootView.findViewById(R.id.total_likes_value);
-        totalComments = (TextView) rootView.findViewById(R.id.total_comments_value);
-        //For Mutual
-        mutualLikes = (TextView) rootView.findViewById(R.id.mutual_likes_value);
-        mutualComments = (TextView) rootView.findViewById(R.id.mutual_comments_value);
-        /**
-         * Localizing
-         */
-        //For  likes
-        fanLikes = (TextView) rootView.findViewById(R.id.fans_likes_value);
-        fanComments = (TextView) rootView.findViewById(R.id.fans_comments_value);
-        //For Follows:
-        followsLikes = (TextView) rootView.findViewById(R.id.follows_likes_value);
-        followsComments = (TextView) rootView.findViewById(R.id.follows_comments_value);
-        //For Strangers:
-        strangerLikes = (TextView) rootView.findViewById(R.id.strangers_likes_value);
-        strangerComments = (TextView) rootView.findViewById(R.id.strangers_comments_value);
 
-        fansCount = (TextView) rootView.findViewById(R.id.fans_value);
-        followsCount = (TextView) rootView.findViewById(R.id.follows_value);
-        mutualCount = (TextView) rootView.findViewById(R.id.mutual_value);
+        mBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_overview,container,false);
+        View view = mBinding.getRoot();
+
         setValues();
-        return rootView;
+        return view;
     }
 
     //Set the values of text views of the fragment
     public static void setValues(){
 
-        totalPosts.setText("("+GlobalVar.mediaDao.getTotalPosts()+")");
-        totalLikes.setText(GlobalVar.mediaDao.getTotalLikes()+"");
-        totalComments.setText(GlobalVar.mediaDao.getTotalComments()+"");
+        mBinding.totalPosts.setText("("+GlobalVar.mediaDao.getTotalPosts()+")");
+        mBinding.totalLikesValue.setText(GlobalVar.mediaDao.getTotalLikes()+"");
+        mBinding.totalCommentsValue.setText(GlobalVar.mediaDao.getTotalComments()+"");
 
-        mutualLikes.setText(GlobalVar.mediaDao.getMutualLikes()+"");
-        mutualComments.setText(GlobalVar.mediaDao.getMutualComments()+"");
+        mBinding.mutualLikesValue.setText(GlobalVar.mediaDao.getMutualLikes()+"");
+        mBinding.mutualCommentsValue.setText(GlobalVar.mediaDao.getMutualComments()+"");
 
-        fanLikes.setText(GlobalVar.mediaDao.getFanLikes()+"");
-        fanComments.setText(GlobalVar.mediaDao.getFanComments()+"");
+        mBinding.fansLikesValue.setText(GlobalVar.mediaDao.getFanLikes()+"");
+        mBinding.fansCommentsValue.setText(GlobalVar.mediaDao.getFanComments()+"");
 
-        followsLikes.setText(GlobalVar.mediaDao.getFollowsLikes()+"");
-        followsComments.setText(GlobalVar.mediaDao.getFollowsComments()+"");
+        mBinding.followsLikesValue.setText(GlobalVar.mediaDao.getFollowsLikes()+"");
+        mBinding.followsCommentsValue.setText(GlobalVar.mediaDao.getFollowsComments()+"");
 
-        strangerLikes.setText(GlobalVar.mediaDao.getStrangerLikes()+"");
-        strangerComments.setText(GlobalVar.mediaDao.getStrangerComments()+"");
+        mBinding.strangersLikesValue.setText(GlobalVar.mediaDao.getStrangerLikes()+"");
+        mBinding.strangersCommentsValue.setText(GlobalVar.mediaDao.getStrangerComments()+"");
 
-        fansCount.setText(GlobalVar.mediaDao.getFansCount()+"");
-        followsCount.setText(GlobalVar.mediaDao.getFollowsCount()+"");
-        mutualCount.setText(GlobalVar.mediaDao.getMutualCount()+"");
+        mBinding.fansValue.setText(GlobalVar.mediaDao.getFansCount()+"");
+        mBinding.followsValue.setText(GlobalVar.mediaDao.getFollowsCount()+"");
+        mBinding.mutualValue.setText(GlobalVar.mediaDao.getMutualCount()+"");
     }
 
 
